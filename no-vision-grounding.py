54441bbcef0688a7870d6212e6b38b0fdd82b190
BeanHam <binhan96816@gmail.com>
2024-05-21 14:46:33 -0700

    test gpt4

---
+++ no-vision-grounding.py
@@ -0,0 +1,160 @@
+import os
+import torch
+import string
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from PIL import Image
+from matplotlib.patches import Rectangle
+import accelerate
+import bitsandbytes
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+
+stop_line_prompt = """
+[INST] <image>\n
+A stop line is a white line painted on the road at intersections where traffic must stop. 
+It shows drivers where to halt their vehicles. 
+Please identify the bounding box of the stop line in the image in the format of (xtl, ytl, xbr, ybr).[/INST]
+"""
+raised_table_prompt = """
+[INST] <image>\nA raised table in the road is usually painted in white color with two arrows. Please identify the bounding box of the raised table in the image. [/INST]
+"""
+
+def iou_cal(gt, pred):
+    """
+    Calculate IoU metric.
+    """    
+    return np.logical_and(gt, pred).sum()/np.logical_or(gt, pred).sum() 
+
+def write_completion_request(prompt, base64_image):
+    """
+    Compose completion request.
+    """
+    
+    completion = {
+      "model": "gpt-4-turbo-2024-04-09",
+      "messages": [
+          {"role": "user",
+           "content": [
+               {"type": "text", "text": prompt},
+               {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
+           ]}
+      ],
+      "max_tokens": 100
+    }
+    return completion
+
+def post_processing(response, output_path, save_name):
+    """
+    Extract bounding box.
+    """
+    try:
+        response = response.translate(str.maketrans('', '', string.punctuation))
+        xtl, ytl, xbr, ybr = [int(l) for l in response.split() if l.isnumeric()]
+    except:
+        xtl, ytl, xbr, ybr = 0,0,0,0
+    bbox = np.array([xtl, ytl, xbr, ybr])
+    np.save(output_path+save_name+'_bbox.npy', bbox)
+    return bbox
+
+def visualize_bbox(img, bbox, output_path, image_name):
+    """
+    Visualize bbox.
+    """
+    
+    save_dir = output_path+image_name+'_bbox.npy'
+    xtl, ytl, xbr, ybr = bbox
+    plt.imshow(img)
+    plt.hlines(ytl, xmin=xtl, xmax=xbr, color='red')
+    plt.hlines(ybr, xmin=xtl, xmax=xbr, color='red')
+    plt.vlines(xtl, ymin=ytl, ymax=ybr, color='red')
+    plt.vlines(xbr, ymin=ytl, ymax=ybr, color='red')
+    plt.axis('off')
+    plt.savefig(save_dir, dpi=600, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+def generate_masking(img, bbox, output_path, save_name, image_size):
+    
+    """
+    Generate binary masking.
+    """
+    
+    save_dir = output_path+save_name
+    xtl, ytl, xbr, ybr = bbox
+    
+    # plot binary masking
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    ax.axis('off')
+    rect = Rectangle((xtl, ytl), xbr-xtl, ybr-ytl, color="black", angle=0, rotation_point='center')
+    ax.add_patch(rect)
+    plt.savefig(save_dir+'_masking.png', dpi=600, bbox_inches='tight', pad_inches=0)
+    plt.close()
+    
+    # generate binary masking
+    img_masking = np.array(Image.open(save_dir+'_masking.png').convert('RGB').resize(image_size))
+    masking = np.zeros(image_size)
+    masking[np.where(img_masking.sum(axis=-1)==0.0)]=True
+    np.save(save_dir+'_masking.npy', masking)    
+    os.remove(save_dir+'_masking.png')
+    
+def main():        
+        
+    #-------------------------
+    # arguments
+    #-------------------------
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--object', required=True, help='stop_line or raised_table')
+    parser.add_argument('--api_key', required=True, help='stop_line or raised_table')
+    args = parser.parse_args()
+    obj = args.object
+    api_key = args.api_key
+    
+    method='zero-shot'  
+    image_size = (336,336)
+    image_path = f'images/{obj}/'
+    image_names = os.listdir(image_path)
+    image_names = [name for name in image_names if ((name.endswith('.png') & ('masking' not in name)))]
+    output_path = f'outputs/{method}/{obj}/'
+    api_web = "https://api.openai.com/v1/chat/completions"
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
+    if obj == "stop_line":
+        prompt = stop_line_prompt
+    else:
+        prompt = raised_table_prompt        
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+        
+    # ---------------
+    # iterate image
+    # --------------- 
+    for image_name in tqdm(image_names):
+    
+        save_name = image_name.split('.')[0]
+        img = Image.open(image_path+image_name).convert('RGB').resize(image_size)
+        base64_image = encode_image(image_path+image_name)
+        completion = write_completion_request(prompt, base64_image)
+        response = requests.post(api_web, headers=headers, json=completion)
+        response = response.json()['choices'][0]['message']['content']
+        bbox = post_processing(response)               
+        visualize_bbox(img, bbox, output_path, image_name)
+        generate_masking(img, bbox, output_path, save_name, image_size)
+        
+    # --------------------
+    # evaluation
+    # --------------------        
+    files = os.listdir(image_path)        
+    files = [file for file in files if 'masking.npy' in file]
+    iou = []
+    for file in files:
+        gt = np.load(image_path+file)
+        pred = np.load(output_path+file)
+        iou.append(iou_cal(gt,pred))
+    print(f'{obj.upper()}, Zero Shot, IoU: {np.mean(iou)}')
+        
+if __name__ == "__main__":
+    main()
